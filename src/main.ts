a800ac9fca2f61c1dedf4592a9834281ce8bb183
Marco Antonio Manrique Acha <63822742+MarcoMnrq@users.noreply.github.com>
2022-04-10 21:28:47 -0500

    fix: port

--- src/main.ts
+++ src/main.ts
@@ -40,6 +40,6 @@ async function bootstrap() {
   SwaggerModule.setup('api-docs', app, document);
 
   // Start application
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
